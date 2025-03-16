4197c95c5abf9aedd8831501dbbd365171f6960f
DrunkAxolotl <aresgabrielyan@gmail.com>
2025-03-15 23:56:00 -0400

    Unsuccessfully integrated jCards library.

---
+++ Deliverable 3/GamblingSimulator25/GamblingSimulator25/src/main/java/com/sigmatechnologies/gamblingsimulator25/Test.java
@@ -0,0 +1,16 @@
+package com.sigmatechnologies.gamblingsimulator25;
+
+/*
+import io.lyuda.jCards.Card;
+import io.lyuda.jCards.Rank;
+import io.lyuda.jCards.Suit;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Card aceOfSpades = new Card(Rank.ACE, Suit.SPADES);
+        System.out.println(aceOfSpades);
+    }
+
+}
+ */
