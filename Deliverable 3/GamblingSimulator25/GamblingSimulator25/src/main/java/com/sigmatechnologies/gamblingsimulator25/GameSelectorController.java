23a7cba59f41c760c2fce01d39108e29f2d502dd
DrunkAxolotl <aresgabrielyan@gmail.com>
2025-03-16 20:35:06 -0400

    Added roulette UI/controls.

--- Deliverable 3/GamblingSimulator25/GamblingSimulator25/src/main/java/com/sigmatechnologies/gamblingsimulator25/GameSelectorController.java
+++ Deliverable 3/GamblingSimulator25/GamblingSimulator25/src/main/java/com/sigmatechnologies/gamblingsimulator25/GameSelectorController.java
@@ -1,8 +1,14 @@
 package com.sigmatechnologies.gamblingsimulator25;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.Node;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
 import java.util.Objects;
 
 public class GameSelectorController {
@@ -27,6 +33,29 @@ public class GameSelectorController {
         blackjackIV.setImage(blackjackI);
         rouletteIV.setImage(rouletteI);
         dicesIV.setImage(dicesI);
+
+    }
+
+    @FXML
+    protected void onRouletteClick(MouseEvent mouseEvent) {
+
+        Node source = (Node) mouseEvent.getSource();
+
+        try {
+
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/com/sigmatechnologies/gamblingsimulator25/roulette.fxml"));
+            BorderPane root = loader.load();
+
+            Stage stage = (Stage) source.getScene().getWindow();
+
+            Scene newScene = new Scene(root);
+            stage.setScene(newScene);
+
+        } catch (Exception e) {
+
+            e.printStackTrace();
+
+        }
 
     }
 
