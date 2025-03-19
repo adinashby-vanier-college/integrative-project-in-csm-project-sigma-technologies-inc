2efb3ebac4993c39f0261ecd3aa1fce519a88391
Miguel982 <mike.lulic@gmail.com>
2025-03-18 20:39:48 -0400

    idk anymore

--- Deliverable 3/SigmaCasino/src/main/java/com/example/sigmacasino/Poker/HelloApplication.java
+++ Deliverable 3/SigmaCasino/src/main/java/com/example/sigmacasino/Poker/HelloApplication.java
@@ -12,7 +12,6 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         stage.setMaximized(true);
-        System.out.println("in");
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/com/example/sigmacasino/UI/poker.fxml"));
         Parent root = fxmlLoader.load();
         Scene scene = new Scene(root);
