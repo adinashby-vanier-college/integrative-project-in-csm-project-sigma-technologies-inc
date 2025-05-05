8db39039efa80c80a776b5a6e708ff0c00474dbf
Miguel982 <mike.lulic@gmail.com>
2025-05-05 02:00:41 -0400

    idk

--- Deliverable 3/SigmaCasino/src/main/java/com/example/sigmacasino/Menus/SettingsController.java
+++ Deliverable 3/SigmaCasino/src/main/java/com/example/sigmacasino/Menus/SettingsController.java
@@ -1,6 +1,42 @@
 package com.example.sigmacasino.Menus;
 
+import javafx.beans.property.ObjectProperty;
+import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ColorPicker;
+import javafx.scene.control.Slider;
+import javafx.scene.paint.Color;
+
 public class SettingsController {
+    @FXML private CheckBox showUICheckBox;
+    @FXML private Slider soundSlider;
+    @FXML private CheckBox showAnimationsCheckBox;
+    @FXML private ColorPicker backgroundColor;
+
+
+    @FXML
+    private void initialize() {
+        soundSlider.valueProperty().addListener((observable, oldValue, newValue) -> {
+            System.out.println("Slider value changed from " + oldValue + " to " + newValue);
+        });
+    }
+
+    public boolean getUICheckBoxValue(){
+        return showUICheckBox.isSelected();
+    }
+
+    public double getSoundSliderValue(){
+        return soundSlider.getValue();
+    }
+
+    public boolean getAnimationValue(){
+        return showAnimationsCheckBox.isSelected();
+    }
+
+    public ObjectProperty<Color> getBackgroundColor() {
+        return backgroundColor.valueProperty();
+    }
+
 
 
 }
