4ac2ad38f8d0d816bafe43a8f33014e6637865eb
Miguel982 <mike.lulic@gmail.com>
2025-05-05 11:06:25 -0400

    last little comment

--- Deliverable 3/SigmaCasino/src/main/java/com/example/sigmacasino/Settings/Settings.java
+++ Deliverable 3/SigmaCasino/src/main/java/com/example/sigmacasino/Settings/Settings.java
@@ -1,5 +1,6 @@
 package com.example.sigmacasino.Settings;
 
+//Settings class to be serialized into json, add settings as pleased
 public class Settings {
     public boolean isShowUIStats = true;
     public boolean showAnimations = true;
